dbd72bc9cd31479c65e07ff4c7e59c062d689673
Charles <49871679+CharlesCondon@users.noreply.github.com>
2021-01-26 11:40:02 -0500

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    consumer_key:         'vnznOvOTrayZHCOzaxEwIkfwE',
-    consumer_secret:      'eP7IjYKz9U40mbB2hzkQ47sb8VR2INQyU6qwoES7ktH5ofiYRe',
-    access_token:         '1336674571974537216-4RZHjnSZOHWoQIe1iQdINUfIZV8r30',
-    access_token_secret:  'WkTvjkE9jxmCseJxUO7jvoNM2EQXLqKK1h5aVbIN7oAwy',
-}
