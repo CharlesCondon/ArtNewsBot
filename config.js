4b81fea4184b691b9a68aeb8f697d63c7b25de0c
Charles <49871679+CharlesCondon@users.noreply.github.com>
2021-01-27 13:06:53 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    consumer_key:         'API_KEY_HERE',
+    consumer_secret:      'API_SECRET_HERE',
+    access_token:         'ACCESS_KEY_HERE',
+    access_token_secret:  'ACCESS_SECRET_HERE'
+}
