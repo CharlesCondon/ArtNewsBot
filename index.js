63d440f646d46fe4a324f2896e24dfb977f2922f
Charles <49871679+CharlesCondon@users.noreply.github.com>
2021-01-27 13:04:30 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ var config = require('./config');
 
 var T = new Twit(config);
 // scraping function that goes to artnews.com and tweets the top story
-async function scrapeAN (url) {
+async function scrapeAN () {
+    const url = 'https://www.artnews.com';
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto(url);
@@ -20,45 +21,50 @@ async function scrapeAN (url) {
 
     makeTweet(title, hrefs[0]);
 }
-setInterval(scrapeAN('https://www.artnews.com'), 1000*60*60*24);
+scrapeAN();
+setInterval(scrapeAN, (1000*60*60*24));
 
 // scraping function that goes to theartnewspaper.com and tweets the top story
-async function scrapeNewspaper (url) {
+async function scrapeNewspaper () {
+    const url2 = 'https://www.theartnewspaper.com/new-york';
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto(url);
+    await page.goto(url2);
 
     // scrape title of article to use as text in tweet
-    const [first] = await page.$x('/html/body/div[2]/div[2]/div[3]/main/div/div[1]/div[1]/a/text()');
-    const txt = await first.getProperty('textContent');
-    const title = await txt.jsonValue();
+    const [second] = await page.$x('/html/body/div[2]/div[2]/div[3]/main/div/div[1]/div[1]/a/text()');
+    const txt2 = await second.getProperty('textContent');
+    const title2 = await txt2.jsonValue();
 
     // scrape the link that the article is on to direct to in tweet
     const hrefs = await Promise.all((
         await page.$x('/html/body/div[2]/div[2]/div[3]/main/div/div[1]/div[1]/a')).map(async item => await (await item.getProperty('href')).jsonValue()));
 
-    makeTweet(title, hrefs[0]);
+    makeTweet(title2, hrefs[0]);
 }
-setInterval(scrapeNewspaper('https://www.theartnewspaper.com/new-york'), 1000*60*60*24);
+scrapeNewspaper();
+setInterval(scrapeNewspaper, (1000*60*60*24));
 
 // scraping function that goes to ny-artnews.com and tweets the top story
-async function scrapeNYart (url) {
+async function scrapeNYart () {
+    const url3 = 'https://www.ny-artnews.com';
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto(url);
+    await page.goto(url3);
 
     // scrape title of article to use as text in tweet
-    const [first] = await page.$x('//*[@id="pgi600af639162d990017db73c8_0"]/div[2]/div/article/div/a/div[1]/h2/div');
-    const txt = await first.getProperty('textContent');
-    const title = await txt.jsonValue();
+    const [third] = await page.$x('//*[@id="pgi600af639162d990017db73c8_0"]/div[2]/div/article/div/a/div[1]/h2/div');
+    const txt3 = await third.getProperty('textContent');
+    const title3 = await txt3.jsonValue();
 
     // scrape the link that the article is on to direct to in tweet
     const hrefs = await Promise.all((
         await page.$x('//*[@id="pgi600af639162d990017db73c8_0"]/div[2]/div/article/div/a')).map(async item => await (await item.getProperty('href')).jsonValue()));
 
-    makeTweet(title, hrefs[0]);
+    makeTweet(title3, hrefs[0]);
 }
-setInterval(scrapeNYart('https://www.ny-artnews.com'), 1000*60*60*24);
+scrapeNYart();
+setInterval(scrapeNYart, (1000*60*60*24));
 
 // tweet function that connects to Twitter api to send out the tweet
 // and writes to console if the post worked or not
@@ -104,4 +110,3 @@ function makeTweet(title, link) {
         }
     }
 }
-
