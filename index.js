18b064da7c1305690f9b10b97bd07707fa1522bf
Charles <49871679+CharlesCondon@users.noreply.github.com>
2021-02-01 20:27:46 -0500

    Update index.js

--- index.js
+++ index.js
@@ -45,32 +45,34 @@ async function scrapeNewspaper () {
 scrapeNewspaper();
 setInterval(scrapeNewspaper, (1000*60*60*24));
 
-// scraping function that goes to ny-artnews.com and tweets the top story
-async function scrapeNYart () {
-    const url3 = 'https://www.ny-artnews.com';
-    const browser = await puppeteer.launch();
+// scraping function that goes to independent.co.uk/topic/art and tweets the top story
+async function scrapeIndep () {
+    const url3 = 'https://www.independent.co.uk/topic/art';
+    const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
     const page = await browser.newPage();
     await page.goto(url3);
 
     // scrape title of article to use as text in tweet
-    const [third] = await page.$x('//*[@id="pgi600af639162d990017db73c8_0"]/div[2]/div/article/div/a/div[1]/h2/div');
+    const [third] = await page.$x('//*[@id="sectionContent"]/div[1]/div[2]/div/div[1]/div[3]/h2/a');
     const txt3 = await third.getProperty('textContent');
     const title3 = await txt3.jsonValue();
 
     // scrape the link that the article is on to direct to in tweet
     const hrefs = await Promise.all((
-        await page.$x('//*[@id="pgi600af639162d990017db73c8_0"]/div[2]/div/article/div/a')).map(async item => await (await item.getProperty('href')).jsonValue()));
+        await page.$x('//*[@id="sectionContent"]/div[1]/div[2]/div/div[1]/div[3]/h2/a')).map(async item => await (await item.getProperty('href')).jsonValue()));
 
     makeTweet(title3, hrefs[0]);
+    browser.close();
+
 }
-scrapeNYart();
-setInterval(scrapeNYart, (1000*60*60*24));
+scrapeIndep();
+setInterval(scrapeIndep, (1000*60*60*24));
 
 // tweet function that connects to Twitter api to send out the tweet
 // and writes to console if the post worked or not
 function makeTweet(title, link) {
     var tweet = {
-        status: title + link 
+        status: title + " " + link 
     }
     var timeline = {
         screen_name: 'artnewsforyou',
